4b3fce49b0ea5657fa8f2755f85eaecc9c64e08b
Maksim Sumar <klfusdzen@gmail.com>
2023-05-03 21:50:45 +0300

    added new line from new brunch without Poland1.

--- src/Lesson5.java
+++ src/Lesson5.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class Lesson5 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello POOOOOOOLAND");
         System.out.println("Hello Poland from new brunch");
     }
 }
