84cfc35954cab103778b9f7b0c6b92f91e5130c0
Maksim Sumar <klfusdzen@gmail.com>
2023-05-03 21:22:33 +0300

    added new line from new brunch.

--- src/Lesson5.java
+++ src/Lesson5.java
@@ -4,5 +4,6 @@ import java.util.Arrays;
 public class Lesson5 {
     public static void main(String[] args) {
         System.out.println("Hello Poland");
+        System.out.println("Hello Poland from new brunch");
     }
 }
