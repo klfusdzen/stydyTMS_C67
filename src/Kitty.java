716f3977a763435cbfe96eaaa16d4f59fedcd232
Maksim Sumar <klfusdzen@gmail.com>
2023-05-10 19:44:11 +0300

    added new

---
+++ src/Kitty.java
@@ -0,0 +1,10 @@
+
+
+public class Kitty {
+    public static void main(String[] args) {
+        //Чертеж уже есть
+        //Создаем обьект котенок
+        int i = 5;
+        Kitty firstKitty = new Kitty();
+    }
+}
