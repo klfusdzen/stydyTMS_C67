c9f3d1d838fa0e5a5ef648b810617336d33a2d78
Maksim Sumar <klfusdzen@gmail.com>
2023-05-12 11:42:44 +0300

    Finished task

---
+++ src/Homework/homework5/CreditCard.java
@@ -0,0 +1,38 @@
+package Homework.homework5;
+
+//Создать класс CreditCard c полями номер счета, текущая сумма на счету.
+// Добавьте метод, который позволяет начислять сумму на кредитную карточку.
+// Добавьте метод, который позволяет снимать с карточки некоторую сумму.
+// Добавьте метод, который выводит текущую информацию о карточке.
+// Напишите программу, которая создает три объекта класса CreditCard у которых заданы номер счета и начальная сумма.
+//Тестовый сценарий для проверки: Положите деньги на первые две карточки и снимите с третьей.
+// Выведите на экран текущее состояние всех трех карточек.
+
+public class CreditCard {
+    int accountNumber;
+    double currentAmount;
+
+    public CreditCard(int accountNumber, double currentAmount) {
+        this.accountNumber = accountNumber;
+        this.currentAmount = currentAmount;
+    }
+
+    public CreditCard() {
+    }
+
+    double plusMoney(double amount) {
+        return this.currentAmount += amount;
+    }
+
+    double minusMoney(double amount) {
+        if (this.currentAmount >= amount) {
+            return this.currentAmount -= amount;
+        } else {
+            return this.currentAmount;
+        }
+    }
+
+    void showInfo() {
+        System.out.println("Аккаунт: " + this.accountNumber + "; Количество денег : " + this.currentAmount);
+    }
+}
---
+++ src/Homework/homework5/Main.java
@@ -0,0 +1,34 @@
+package Homework.homework5;
+
+import java.util.Scanner;
+//Создать класс CreditCard c полями номер счета, текущая сумма на счету.
+// Добавьте метод, который позволяет начислять сумму на кредитную карточку.
+// Добавьте метод, который позволяет снимать с карточки некоторую сумму.
+// Добавьте метод, который выводит текущую информацию о карточке.
+// Напишите программу, которая создает три объекта класса CreditCard у которых заданы номер счета и начальная сумма.
+//Тестовый сценарий для проверки: Положите деньги на первые две карточки и снимите с третьей.
+// Выведите на экран текущее состояние всех трех карточек.
+
+public class Main {
+    public static void main(String[] args) {
+
+        CreditCard creditCard1 = new CreditCard(345678,20);
+        CreditCard creditCard2 = new CreditCard(234567,30);
+        CreditCard creditCard3 = new CreditCard(123456,40);
+
+        System.out.println("На какую сумму пополнить первую карту? ");
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        creditCard1.plusMoney(a);
+        System.out.println("На какую сумму пополнить вторую карту? ");
+        double b = scanner.nextDouble();
+        creditCard2.plusMoney(b);
+        System.out.println("Какую сумму снять с третьей карты? ");
+        double c = scanner.nextDouble();
+        creditCard3.minusMoney(c);
+
+        creditCard1.showInfo();
+        creditCard2.showInfo();
+        creditCard3.showInfo();
+    }
+}
