7af32ced66d2be4207775e7517946f012f577d00
Maksim Sumar <klfusdzen@gmail.com>
2023-05-22 20:55:11 +0300

    +1 lesson

---
+++ src/Courses/lesson8/HP.java
@@ -0,0 +1,8 @@
+package Courses.lesson8;
+
+public class HP extends Computer{
+    @Override
+    void getClassName() {
+        System.out.println("I’m HP");
+    }
+}
---
+++ src/Courses/lesson8/Mac.java
@@ -0,0 +1,8 @@
+package Courses.lesson8;
+
+public class Mac extends Computer{
+    @Override
+    void getClassName() {
+        System.out.println("I’m Mac");
+    }
+}
