3caac0b810fe72e7690ce83066db8056208c997a
Maksim Sumar <klfusdzen@gmail.com>
2023-05-22 21:40:38 +0300

    +1 lesson

--- src/Courses/lesson8/Main.java
+++ src/Courses/lesson8/Main.java
@@ -2,8 +2,8 @@ package Courses.lesson8;
 
 public class Main {
     public static void main(String[] args) {
-        Computer mac = new Mac(4000);
-        Computer hp = new HP(1800);
+        Mac mac = new Mac(4000);
+        HP hp = new HP(1800);
         mac.getClassName();
         hp.getClassName();
     }
