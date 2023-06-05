97d5855154f837ebe421bdde08ab7a82442603f2
Maksim Sumar <klfusdzen@gmail.com>
2023-06-05 14:14:23 +0300

    +1 homework

---
+++ src/Homework/homework11/Main.java
@@ -0,0 +1,19 @@
+package Homework.homework11;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the line:");
+        String input = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile("[A-Z]{2,6}");
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()){
+            System.out.println(matcher.group());
+        }
+    }
+}
